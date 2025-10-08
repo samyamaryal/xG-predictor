effb8d23084970342cd2c04eb5b3a686d241438c
samyamaryal <samyam.aryal01@gmail.com>
2025-10-08 16:04:21 -0500

    Added code to scrape the data directory and collect training clips with shot labels

--- shot_data_extraction.py
+++ shot_data_extraction.py
@@ -4,12 +4,9 @@ import torchvision
 import numpy as np
 import json
 import cv2
+import os
 
 INPUT_NO_OF_FRAMES = 10
-
-with open("data/SoccerNet/england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1 Burnley/Labels-v2.json", "r") as file:
-    data = json.load(file)
-
 
 # All shots from the game, labels extracted from JSON 
 
@@ -19,27 +16,37 @@ with open("data/SoccerNet/england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1
 def time_converter(time_in):
     half, time = time_in.split("-") # Half and time are split on this
     mins, seconds = time.split(":")
-    timestamp_seconds = mins*60+seconds 
-    return half, timestamp_seconds
+    timestamp_seconds = int(mins)*60+int(seconds) 
+    return half.strip(), timestamp_seconds
+
 # For all shots: 
 ls = []
 y = []
 
 
-def extract_clips():
+def extract_shots(game_path):
+    with open(f'{game_path}/Labels-v2.json', "r") as file:
+        data = json.load(file)
+
     for item in data['annotations']:
         clip = []
         if "Shot" in item['label']:
 
             # Get half and time in seconds
             half_number, time_in_seconds = time_converter(item['gameTime']) # time_converter returns the half that the game was played in, and the time of an event in seconds
-            
+            # print("Half number and time extracted")
+
             # Generate source file name from the above video
             filename = f'{half_number}_720p.mkv'
 
+            video_path = f'{game_path}/{filename}'
+            # print("Video path", video_path)
+
+            if not os.path.exists(video_path):
+                raise FileNotFoundError(f"{video_path} does not exist")
 
             # Read video to get shot clip 
-            cap = cv2.VideoCapture(f"data/SoccerNet/england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1 Burnley/{filename}")
+            cap = cv2.VideoCapture(video_path)
             fps = cap.get(cv2.CAP_PROP_FPS)
 
             # Since we're reading frame-by-frame, we need the starting frame number. 
@@ -54,12 +61,49 @@ def extract_clips():
 
 
             clip_array = np.stack(clip) # Shape 10, (l, w, c)
-            print(clip_array.shape)
             
 
             # frames_list.append(frames)
             y.append(1) # Append label to the data
             ls.append(clip_array)
+
+
+def crawl_dataset(root_dir):
+
+    for league_name in os.listdir(root_dir):
+        league_path = os.path.join(root_dir, league_name)
+        print("\nLeague Path: ", league_path)
+        if not os.path.isdir(league_path):
+            continue
+        
+        for season in os.listdir(league_path):
+            season_path = os.path.join(league_path, season)
+            print("- Season Path: ", season_path)
+            if not os.path.isdir(season_path):
+                continue
+            
+            for game_name in os.listdir(season_path):
+                game_path = os.path.join(season_path, game_name)
+                print("-- Game Path: ", game_path)
+                if not os.path.isdir(game_path):
+                    continue
+
+                if os.path.exists(game_path):
+                    print(f"--- Processing: {game_path} ---")
+                    try:
+                        extract_shots(game_path)
+                        print(f"--- Processed {game_path} ---")
+                    except Exception as e:
+                        print(f"--- Error processing {game_path}: {e} ---")
+                else:
+                    print(f"-- Missing files in {game_path} --")
+        break
+
+
+if __name__=='__main__':
+    print("Current working directory:", os.getcwd())
+
+    crawl_dataset('data/SoccerNet') # the data is placed inside data/SoccerNet directory
 
     # We need to stack the training data into a numpy array. However, this can be done once all the frames have been extracted.
     # train_data = np.stack(ls) 
